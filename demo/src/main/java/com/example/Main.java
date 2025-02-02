99ff2af4c9ff604a3f5aa7aa6ae17f8fd68a7b43
Dany <daaldy4@gmail.com>
2025-02-01 19:48:11 -0600

    parser LL1

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,6 +2,25 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] testInputs = {
+            "bin a; oct b; hex c; a = 101 + 11; b = 7 * 2; c = A + 1;",
+            "bin x; oct y; hex z; x = 110; y = 5 + 3; z = F * 2;",
+            "bin p; oct q; hex r; p = 1001 - 1; q = 6 / 2; r = B + 4;"
+        };
+
+        for (String input : testInputs) {
+            try {
+                System.out.println("Parsing input: " + input);
+                Lexer lexer = new Lexer(input);
+                Parser parser = new Parser(lexer);
+                parser.parse();
+                System.out.println("\n");
+            } catch (RuntimeException e) {
+                System.out.println("Error: " + e.getMessage() + "\n");
+            }
+        }        
+        
     }
+    
 }
+
